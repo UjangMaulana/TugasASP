66c603620377aa142d467f1f65b5ac0f5d92ae39
UjangMaulana <ujangmaulana182@gmail.com>
2021-11-29 20:40:49 +0700

    p

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+namespace Absensi.Models
+{
+    public class User
+    {
+        public int ID{get;set;}
+
+        public string Email{get;set;}
+
+        public string Password{get;set;}
+
+        public string Message{get;set;}
+    }
+}
